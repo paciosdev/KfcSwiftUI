e1f3e823ba9e8fa4e5a5c713ee2448a48e7ed47d
Francesco Paciello <kekkopacio@gmail.com>
2021-05-17 13:49:13 +0200

    Initial Commit

---
+++ kfc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kfc
+//
+//  Created by Kekko Paciello on 17/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kfc/kfcApp.swift
@@ -0,0 +1,17 @@
+//
+//  kfcApp.swift
+//  kfc
+//
+//  Created by Kekko Paciello on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct kfcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
